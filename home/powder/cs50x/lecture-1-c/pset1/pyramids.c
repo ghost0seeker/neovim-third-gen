076d1e13be88b63d21bc28386e9f588526815054
piyush <piyushkumar16@protonmail.com>
2025-08-01 21:24:26 +0530

    Completed cs50x lecture 1 pset1 mario.c

---
+++ home/powder/cs50x/lecture-1-c/pset1/pyramids.c
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int get_positive_int(void);
+void rhs_pyramid(int height);
+void lhs_pyramid(int height);
+void pyramid(int height);
+
+
+int main(void)
+{
+    int height = get_positive_int();
+    pyramid(height);
+    
+}
+
+int get_positive_int(void)
+{
+    int height;
+
+    do 
+    {
+        height = get_int("Height: ");
+
+    }
+    while (height < 1);
+    return height;
+}
+
+void rhs_pyramid(int height)
+{
+    int column = 1;
+
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            printf("#");
+        }
+        printf("\n");
+        column++;
+    }
+}
+
+void lhs_pyramid(int height)
+{
+    int column = 1;
+    int whitespace = height - 1;
+
+    for (int i = 0; i < height; i++)
+    {
+        for (int w = 0; w < whitespace; w++)
+        {
+            printf(" ");
+        }
+        for (int j = 0; j < column; j++)
+        {
+            printf("#");
+        }
+        printf("\n");
+        whitespace--;
+        column++;
+    }
+}
+
+void pyramid(int height)
+{
+    int column = 1;
+    int whitespace = height - 1;
+
+    for (int i = 0; i < height; i++)
+    {
+        for (int w = 0; w < whitespace; w++)
+        {
+            printf(" ");
+        }
+        for (int j = 0; j < column; j++)
+        {
+            printf("#");
+        }
+        printf("  ");
+
+        for (int k = 0; k < column; k++)
+        {
+            printf("#");
+        }
+
+        printf("\n");
+        whitespace--;
+        column++;
+    }
+}
