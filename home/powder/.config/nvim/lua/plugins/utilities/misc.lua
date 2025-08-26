9e5267de56a2ec7db282e4d1c9d42e1323ed3b8d
Piyush <piyushkumar16@protonmail.com>
2025-08-26 19:07:18 +0530

    added windwp/nvim-autopairs for {}

--- home/powder/.config/nvim/lua/plugins/utilities/misc.lua
+++ home/powder/.config/nvim/lua/plugins/utilities/misc.lua
@@ -8,4 +8,12 @@ return {
     "MunifTanjim/nui.nvim",
     lazy = true, -- Optional: Set to true if you want to load it lazily
   },
+
+				{
+				'windwp/nvim-autopairs',
+				event = "InsertEnter",
+				config = true
+				-- use opts = {} for passing setup options
+				-- this is equivalent to setup({}) function
+				}
 }
